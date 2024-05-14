9ecfa26f54744ee26a2908023cef8f012252da33
emimeiKly <a925127935@gmail.com>
2024-05-14 21:40:47 +0800

    Initial commit

---
+++ src/main/java/com/ccc/fivedata/FivedataApplication.java
@@ -0,0 +1,13 @@
+package com.ccc.fivedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivedataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FivedataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ccc/fivedata/FivedataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccc.fivedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FivedataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
